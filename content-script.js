0dd3b6f3d16a477bf3766f5eafdb4f01b1108fd9
bati11 <mail.bati11@gmail.com>
2022-06-17 14:49:06 +0900

    support click event

--- content-script.js
+++ content-script.js
@@ -3,7 +3,19 @@ const style = `<style>
     border: 3px solid #30a14e;
     width: 30px;
     height: 30px;
+    cursor: pointer;
   }
 </style>`
 document.querySelector(`head`).insertAdjacentHTML('beforeend', style);
+document.addEventListener('click', function(event) {
+  if (event.target && event.target.classList.contains("octicon-git-pull-request") && event.target.classList.contains("open")) {
+    const q = document.getElementById("js-issues-search");
+    if (q != null) {
+      if (!q.value.includes("-is:draft")) {
+        q.value = q.value + " -is:draft";
+      }
+      q.parentElement.submit();
+    }
+  }
+});
 
