7cfd3195a17650174a154c737f4d54fc521bd166
bati11 <mail.bati11@gmail.com>
2022-06-17 12:21:21 +0900

    change from background to content-script

---
+++ content-script.js
@@ -0,0 +1,9 @@
+const style = `<style>
+  .octicon-git-pull-request.open {
+    border: 3px solid #30a14e;
+    width: 30px;
+    height: 30px;
+  }
+</style>`
+document.querySelector(`head`).insertAdjacentHTML('beforeend', style);
+
